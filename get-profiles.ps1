403058b7f657817b60c2dfdb2a24c63d1f90ddfe
Karl Calden <kcalden323@gmail.com>
2020-11-16 19:52:06 -0500

    Create release directory before zipping

--- get-profiles.ps1
+++ get-profiles.ps1
@@ -2,6 +2,8 @@ git clone -b fm1-profile "https://github.com/Fablicator/KISSlicerDist.git"  ".\p
 git clone -b mx-profile "https://github.com/Fablicator/KISSlicerDist.git"  ".\profiles\mx\"
 git clone -b sx-profile "https://github.com/Fablicator/KISSlicerDist.git"  ".\profiles\sx\"
 
+mkdir "release"
+
 Compress-Archive -Path ".\profiles\mx\*" -DestinationPath ".\release\mx-profile.zip"
 Compress-Archive -Path ".\profiles\sx\*" -DestinationPath ".\release\sx-profile.zip"
 Compress-Archive -Path ".\profiles\fm1\*" -DestinationPath ".\release\fm1-profile.zip"
